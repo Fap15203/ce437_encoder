8b08ff8208a19a9676338252609ca818301c7a9c
Fap15203 <42090475+Fap15203@users.noreply.github.com>
2024-01-05 10:45:48 +0700

    Update func_call.c

--- func_call.c
+++ func_call.c
@@ -11,7 +11,7 @@
     /* USER CODE BEGIN 3 */
 	for (speed = 0; speed < 100; speed = speed + 10) {
 	  HAL_Delay(1000);
-	  __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_1, 20); // xuat xung PWM
+	  __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_1, speed); // xuat xung PWM
 	  encoder_cnt = __HAL_TIM_GET_COUNTER(&htim2);
 	  rpm = (encoder_cnt - encoder_cnt_pre) / 1200; // uoc luong 20(dpg) * 60(s)
 	  encoder_cnt_pre = encoder_cnt;
